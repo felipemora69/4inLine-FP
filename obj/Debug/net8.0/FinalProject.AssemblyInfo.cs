2b97989a3c80de4ea9245ab43bf7a40d9406afdf
Felipe Mora <felipe.moraa13@gmail.com>
2024-04-21 13:33:00 -0600

    Update

--- obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinalProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4dd556121631bf409a40ccb927963320bfbd551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a487f67ce5a9d7f8444cec5bd5c92e869371eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinalProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
